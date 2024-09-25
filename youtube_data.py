a3cc7d76e557a50fec61ef00cfe69c8fec96bec2
Bilal Qamar <bilalqamar229@gmail.com>
2024-09-25 11:31:40 -0700

    found the views of the vedio

--- youtube_data.py
+++ youtube_data.py
@@ -1,10 +1,13 @@
-import requests as re
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
+url = 'https://www.youtube.com/playlist?list=PLu71SKxNbfoBsMugTFALhdLlZ5VOqCg2s'
+browser= webdriver.Chrome()
+browser.get(url)
+html_text = browser.page_source
+soup = BeautifulSoup(html_text, 'html.parser')
+# print(html_text)
+vedios_list = soup.find('div', class_= 'style-scope ytd-playlist-video-renderer')
+meta_data = vedios_list.find('h3', class_= 'style-scope ytd-playlist-video-renderer')
 
-html_text = re.get('https://www.youtube.com/playlist?list=PLu71SKxNbfoBsMugTFALhdLlZ5VOqCg2s').text
-# soup = BeautifulSoup(html_text, 'lxml')
-print(html_text)
-# vedios_list = soup.find('div', class_= ' style-scope ytd-playlist-video-list-renderer style-scope ytd-playlist-video-list-renderer')
-
-# print(vedios_list)
+print(meta_data['aria-label'])
